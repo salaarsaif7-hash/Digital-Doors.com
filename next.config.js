f2e2e89a42b69b73ba09dadd82fa35534efaaf2f
Subhan ibn yousaf <subhanofficial2k5@gmail.com>
2026-01-27 17:43:17 +0500

    update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "jollymod.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'jollymod.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
